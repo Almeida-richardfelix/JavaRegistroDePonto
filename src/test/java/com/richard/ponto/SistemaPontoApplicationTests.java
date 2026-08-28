7ecc8bfc90ed450099d031646b084b63c274843e
Richard Felix <165296707+Almeida-richardfelix@users.noreply.github.com>
2026-08-28 18:41:39 +0000

    chore: cria estrutura inicial do Spring Boot

---
+++ src/test/java/com/richard/ponto/SistemaPontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richard.ponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
